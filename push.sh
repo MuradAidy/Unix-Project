34824e6c7a433e5e9109ce6df715d47ad0c1ed05
muradaydi <murad@gmail.com>
2025-12-25 19:13:56 +0000

    uploading project files to repo

---
+++ push.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+msg="$1"
+
+cd "$(dirname "$0")"
+
+if [ ! -d ".git" ];
+then
+  git init
+  git branch -M main
+  git remote add origin https://github.com/MuradAidy/Unix-Project.git 2>/dev/null || true
+  echo "Git repo ready."
+fi
+
+git add .
+
+if git diff --cached --quiet; then
+  echo "No changes."
+  exit 0
+else
+  git commit -m "$msg"
+fi
+
+git push origin main
+echo "Pushed"
